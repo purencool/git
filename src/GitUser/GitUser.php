e076616057ba6feeeba9475564bb60a2b099afb4
Purencool <purencool@gmail.com>
2014-04-26 18:21:49 +1000

    Testing namespacing

---
+++ src/GitUser/GitUser.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *  Description
+ *
+ *  @link
+ *  @version
+ *  @author    Purencool Website Development <purencool at gmail dot com>
+ *  @license   GPLV3
+ *
+ *  Documentation:
+ *  Tickets:
+ */
+namespace GitUser;
+
+class GitUser
+{
+  public function __toString()
+  {
+    return "User\GitUser";
+  }
+}
+
