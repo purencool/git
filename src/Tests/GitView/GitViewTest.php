e1e4c433f80fb817468145908be9bbcbf1cca58d
Purencool <purencool@gmail.com>
2014-04-29 17:00:47 +1000

    This refs #4

--- src/Tests/GitView/GitViewTest.php
+++ src/Tests/GitView/GitViewTest.php
@@ -2,13 +2,18 @@
 
 namespace GitView\Tests;
 
+use \ __ ; //-- underscore php
+
 class GitViewTest extends \PHPUnit_Framework_TestCase
 {
 
-  public function testTrueIsTrue()
+  /**
+  * Testing to see if underscore works so it can be used
+  */
+  public function testUnderScore()
   {
-    $foo = false;
-    $this->assertTrue($foo);
+     //-- This test came from underscore on github
+     $this->assertEquals(1, __::first(array(1,2,3)), 'can pull out the first element of an array');
   }
 
   /**
