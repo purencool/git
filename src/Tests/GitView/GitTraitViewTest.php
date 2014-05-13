4e7f3dcf48c370183d0338942dd393402e311776
Purencool <purencool@gmail.com>
2014-05-13 17:00:13 +1000

    This refs #7

--- src/Tests/GitView/GitTraitViewTest.php
+++ src/Tests/GitView/GitTraitViewTest.php
@@ -9,52 +9,107 @@ class GitTraitViewTest extends \PHPUnit_Framework_TestCase
   use \GitView\GitTraitView;
   
   /**
-   *  access to trait toString 
+   *  Access to trait testing to see if pre string
+   *  works
    *
    *  @return string
    */
-   public function testExecutableShell()
+   public function testViewPre()
    {
+     $string = "This is a string and it";
+     $test = self::viewPre($string);
      
-     $this->assertEquals();
-     reself::viewPre(shell_exec('ls -lart'));
+     //-- Test string
+     $this->expectOutputString($test);
+     
+     //-- Test for pre tag
+     $tag = array('tag' => 'pre');
+     $this->assertTag($tag, $test);
+     print $test;
    }
    
+  /**
+   * Returning a string wrapped in pre tags
+   */
+   public function testViewDiffPre()
+   {
+     //-- Test for string pre
+     $string = "Testing1\r\n"."Testing2\r\n"."Testing3\r\n";
+     $test = self::viewDiff($string, TRUE);
+     
+      //-- Test string
+     $this->expectOutputString($test);
+     
+     //-- Test for pre tag
+     $tag = array('tag' => 'pre');
+     $this->assertTag($tag, $test);
+     print $test;    
+   } 
    
   /**
-   *  self::viewPre() 
-   *
-   *  @return string
+   * Returning a string wrapped in div tags
    */
- // public function testExecutableShell()
- // {
- //   reself::viewPre(shell_exec('ls -lart'));
- // }
+   public function testViewDiffDiv()
+   {
+     //-- Test for string
+     $string = "Testing1\r\n"."Testing2\r\n"."Testing3\r\n";
+     $test = self::viewDiff($string, FALSE);
+     
+     //-- Test string
+     $this->expectOutputString($test);
+     
+     //-- Test for pre tag
+     $tag = array('tag' => 'div');
+     $this->assertTag($tag, $test); 
+     print $test;       
+   }
+   
+  /**
+   * Test for removed class in div tag
+   */
+   public function testViewDiffRemovedClass()
+   {
+     //-- Test for string
+     $string = "---Testing1\r\n  -Testing1\r\n";
+     $test = self::viewDiff($string, FALSE);
+     $tag = array(
+      'tag' => 'div',
+      'attributes' => array('class' => 'removed')
+     );
+     $this->assertTag($tag, $test); 
+     print $test;       
+   }
 
   /**
-   *  This method adds to GITDIFF constant into exec shell
-   *
-   *  self::viewDiff() is a trait that formats the
-   *  output into html tags
-   *
-   *  @return string
+   * Test for added class in div tag
    */
-  // public function testGitDiff()
-  // {
-  //   $this->expectOutputString(self::viewDiff(array(1,2,3,4)));
-  // }
-
+   public function testViewDiffAddedClass()
+   {
+     //-- Test for string
+     $string = "+++Testing1\r\n  +Testing1\r\n";
+     $test = self::viewDiff($string, FALSE);
+     $tag = array(
+      'tag' => 'div',
+      'attributes' => array('class' => 'added')
+     );
+     $this->assertTag($tag, $test); 
+     print $test;       
+   }
+   
   /**
-   *  This method adds to GITLOG constant into exec shell
-   *
-   *  self::viewPre() is a trait that formats the
-   *  output in pre tags
-   *
-   *  @return string
+   * Test for none class in div tag
    */
-//  public function testGitLog()
-//  {
-//    return self::viewPre(shell_exec(self::GITLOG));
-//  }
+   public function testViewDiffNoneClass()
+   {
+     //-- Test for string
+     $string = "Testing1\r\n  Testing1\r\n";
+     $test = self::viewDiff($string, FALSE);
+     $tag = array(
+      'tag' => 'div',
+      'attributes' => array('class' => 'none')
+     );
+     $this->assertTag($tag, $test); 
+     print $test;       
+   }
 }
 
